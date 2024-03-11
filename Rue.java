00aa0c56dfc1f08c072367174070736d01081a4f
FaraDuMatin <118701688+FaraDuMatin@users.noreply.github.com>
2024-03-11 12:54:48 -0400

    Add files via upload

---
+++ Rue.java
@@ -0,0 +1,77 @@
+/*
+    Hervé Ng’isse, Matricule : 20204609
+    Farah Mohamed, Matricule: 20246646
+*/
+public class Rue implements Comparable<Rue>{
+    private String nomArete;
+    private Sommet sommetDepart;
+    private Sommet sommetArrivee;
+    private int poidsArete;
+    private boolean visited;
+    // une rue est une arête
+    public Rue(String nomArete, Sommet sommetDepart, Sommet sommetArrivee, int poidsArete) {
+        this.nomArete = nomArete;
+        this.sommetDepart = sommetDepart;
+        this.sommetArrivee = sommetArrivee;
+        this.poidsArete = poidsArete;
+    }
+
+    public boolean isVisited() {
+        return visited;
+    }
+
+    public void setVisited(boolean visited) {
+        this.visited = visited;
+    }
+
+    public String getNomArete() {
+        return nomArete;
+    }
+
+    public Sommet getSommetDepart() {
+        return sommetDepart;
+    }
+
+    public void setSommetDepart(Sommet sommetDepart) {
+        this.sommetDepart = sommetDepart;
+    }
+
+    public Sommet getSommetArrivee() {
+        return sommetArrivee;
+    }
+
+    public void setSommetArrivee(Sommet sommetArrivee) {
+        this.sommetArrivee = sommetArrivee;
+    }
+
+    public int getPoidsArete() {
+        return poidsArete;
+    }
+
+    //comparateur pour la priorityqueue
+    @Override
+    public int compareTo(Rue other) {
+        int weightComparison = Integer.compare(this.poidsArete, other.poidsArete);
+        if (weightComparison != 0) {
+            return weightComparison;
+        } else {
+            int sommetArriveeComparison = this.sommetArrivee.getSommet().compareTo(other.sommetArrivee.getSommet());
+
+            if (sommetArriveeComparison != 0) {
+                return sommetArriveeComparison;
+            } else {
+                return this.sommetDepart.getSommet().compareTo(other.sommetDepart.getSommet());
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Rue{" +
+                "nomArete='" + nomArete + '\'' +
+                ", sommetDepart=" + sommetDepart.getSommet() +
+                ", sommetArrivee=" + sommetArrivee.getSommet() +
+                ", poidsArete=" + poidsArete +
+                '}';
+    }
+}
---
+++ Sommet.java
@@ -0,0 +1,55 @@
+/*
+    Hervé Ng’isse, Matricule : 20204609
+    Farah Mohamed, Matricule: 20246646
+*/
+import java.util.List;
+
+public class Sommet  {
+    private String sommet;
+    private boolean visited;
+
+    private List<Rue> rueConnecte;
+
+    public Sommet() {
+    }
+
+    public Sommet(String sommet, boolean visited, List<Rue> rueConnecte) {
+        //nom du sommet
+        this.sommet = sommet;
+        this.visited = visited;
+        this.rueConnecte = rueConnecte;
+    }
+
+    public String getSommet() {
+        return sommet;
+    }
+    public List<Rue> getRueConnecte() {
+        return rueConnecte;
+    }
+
+
+    public boolean isVisited() {
+        return visited;
+    }
+
+    public void setVisited(boolean visited) {
+        this.visited = visited;
+    }
+
+    public Sommet(String sommet, boolean visited) {
+        this.sommet = sommet;
+        this.visited = visited;
+    }
+    public void addRue(Rue rue){
+        rueConnecte.add(rue);
+    }
+
+    @Override
+    public String toString() {
+        return "Sommet{" +
+                "sommet='" + sommet + '\'' +
+                ", visited=" + visited +
+                ", rueConnecte=" + rueConnecte +
+                '}';
+    }
+}
